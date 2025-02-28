fcd9c033a93a91484570b11a7f5bd2c5433071d6
Ivan <mayhemk57@gmail.com>
2025-02-28 16:30:29 -0700

    Add HTML template for notes display and update index.html structure

--- app/models/Notes.js
+++ app/models/Notes.js
@@ -10,4 +10,16 @@ export class Notes {
     this.createdAt() = new Date()
     this.lastEdited() = new Date()
   }
+
+  get listHTMLTemplate() {
+    return `
+      <div class="card bg-purpdark text-light mb-2" role="button">
+        <div class="d-flex justify-content-between card-header">
+          <div class="fw-bold">Text Name 1</div>
+          <div>Date Created</div>
+        </div>
+        <div class="card-body text-start">Text body field 1</div>
+      </div>
+    `
+  }
 }
