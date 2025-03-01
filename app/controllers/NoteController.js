28b3f2f6995b8e154a50a9f704918f7d9d1506ab
Ivan <mayhemk57@gmail.com>
2025-02-28 17:13:21 -0700

    invoke draw cards

--- app/controllers/NoteController.js
+++ app/controllers/NoteController.js
@@ -1,10 +1,20 @@
-
+import { AppState } from "../AppState.js";
 
 
 
 export class NoteController {
   constructor() {
     console.log('note controller loaded');
+    this.drawNoteFiles()
+    console.log('help');
+
+  }
+  drawNoteFiles() {
+    const noteFiles = AppState.notesFiles
+    let noteFilesContent = ''
+    noteFiles.forEach(noteFile => noteFilesContent += noteFile.listHTMLTemplate)
+    const noteFilesListElem = document.getElementById('noteCard')
+    noteFilesListElem.innerHTML = noteFilesContent
 
   }
 }
--- app/models/Notes.js
+++ app/models/Notes.js
@@ -2,23 +2,23 @@ import { generateId } from "../utils/GenerateId.js";
 
 
 export class Notes {
-  /**@param {{ title: string; content: string}} */
+  /**@param {{ title: string; content: string}} data*/
   constructor(data) {
     this.title = data.title
     this.content = data.content
     this.id = generateId()
-    this.createdAt() = new Date()
-    this.lastEdited() = new Date()
+    // this.createdAt() = new Date()
+    // this.lastEdited() = new Date()
   }
 
   get listHTMLTemplate() {
     return `
       <div class="card bg-purpdark text-light mb-2" role="button">
         <div class="d-flex justify-content-between card-header">
-          <div class="fw-bold">Text Name 1</div>
+          <div class="fw-bold">${this.title}</div>
           <div>Date Created</div>
         </div>
-        <div class="card-body text-start">Text body field 1</div>
+        <div class="card-body text-start">${this.content}</div>
       </div>
     `
   }
